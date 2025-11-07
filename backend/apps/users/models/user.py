24725eff48c0368c99e92db47009b53498060508
Fabio Cordoba <fabiocordoba1@gmail.com>
2025-11-07 15:28:57 -0500

    feat: create user relationship and document type

--- backend/apps/users/models/user.py
+++ backend/apps/users/models/user.py
@@ -2,12 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from apps.core.models.base_model import BaseModel
+from apps.document_type.models.document_type import DocumentType
 
 
 class User(BaseModel, AbstractUser):
     ROLES = (
         ("administrator", "Administrator"),
         ("guest", "Guest"),
+    )
+    document_type = models.ForeignKey(
+        DocumentType,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+    document_number = models.CharField(
+        max_length=50,
+        unique=True,
+        null=True,
+        blank=True,
     )
     email = models.EmailField(unique=True)
     rol = models.CharField(max_length=13, choices=ROLES)
